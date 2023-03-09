2127652ab90c09585c6b3edea4ce47eb7ee9b176
ArmandoSandino <jsandino@arkkosoft.net>
2023-03-08 23:11:09 -0600

    chore; add menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MovieComponent } from './components/movie/movie.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieComponent
+    MovieComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
